6ab8263b9b64743e3afd9ef899cc61e859dbf7fa
CharlesKosUNSW <z5160675@ad.unsw.edu.au>
2022-06-22 13:39:55 +1000

    q1 done

---
+++ Week4Tutorial/Q1.c
@@ -0,0 +1,13 @@
+// Charles Kos
+// 22.06.2022
+// z5160675@unsw.edu.au
+// Main function for running the tutorial questions
+
+// STUDENTS IGNORE THIS (it lets Visual Studio run scanf() without an error)
+#ifdef _MSC_VER
+#define _CRT_SECURE_NO_WARNINGS
+#endif
+
+void q1(void) {
+
+}
---
+++ Week4Tutorial/w4Main.c
@@ -0,0 +1,20 @@
+// Charles Kos
+// 22.06.2022
+// z5160675@unsw.edu.au
+// Main function for running the tutorial questions
+
+// STUDENTS IGNORE THIS (it lets Visual Studio run scanf() without an error)
+#ifdef _MSC_VER
+#define _CRT_SECURE_NO_WARNINGS
+#endif
+
+#include <stdio.h>
+
+void q1(void);
+
+int main(void) {
+
+	//q1();
+
+	return 0;
+}
